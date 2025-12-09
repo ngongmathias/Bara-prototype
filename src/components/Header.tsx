7b2766d21107008cc67017ce5228b010c1ee9efd
ngongmathias <mathiasngongngai@gmail.com>
2025-12-09 03:30:58 +0200

    Test build: intentionally spell 'Connect' as 'Conect' in header footer copy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -747,7 +747,7 @@ export const Header = () => {
               {/* Mobile Menu Footer */}
               <div className="p-4 border-t border-gray-200">
                 <div className="text-xs text-gray-500 font-roboto text-center">
-                  BARA App - Connect with Local Businesses
+                  BARA App - Conect with Local Businesses
                 </div>
               </div>
             </div>
